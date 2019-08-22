6b475a49b4066aae71d2746087404c613cfd1f3a
gtfarng <5735512002@psu.ac.th>
2019-08-22 17:02:55 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Backend from './Components/Backend'
 import Frontend from './Components/Frontend'
 import Index from './Components/Index'
 import Home from './Components/Home'
+import Assignment from './Components/Assignment'
 
 function App() {
   return (
@@ -20,27 +21,36 @@ function App() {
               <Link to="/index"> INDEX</Link> |
               <Link to="/api/"> API</Link> |
               <Link to="/web/"> WEB</Link> |
+              <Link to="/backend"> BACKEND</Link> |
+              <Link to="/frontend"> FRONTEND</Link> |
+              <Link to="/assignment"> ASSIGNMENT</Link> |
+
+
        </h5> </nav>
 
         <Route path="/" exact component={Home} />
         <Route path="/index/" component={Index} />
         <Route path="/api/" component={Backend} />
         <Route path="/web/" component={Frontend} />
+        <Route path="/backend/" component={Backend} />
+        <Route path="/frontend/" component={Frontend} />
+        <Route path="/assignment/" component={Assignment} />
       </div>
     </Router>
 
 <div>
 
 </div>
-
+<br/>
+  <header className="App-header">
 <footer class="page-footer font-small blue pt-4">
 
   <div class="footer-copyright text-center py-3">Copyright © 2019 :
     <a href="mailto:jatupat.pn@gmail.com"> Jatupat.pn@gmail.com</a>
   </div>
 
-</footer>
-
+</footer><br/>
+</header>
     </div>
   );
 }
---
+++ frontend/src/Components/Assignment.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../App.css';
+
+function Assignment() {
+  return (
+    <div className="App"><br/>
+<h3>Backend</h3>
+    <h5>
+    - Create a new controller called “SCG”
+<br/>- X, 5, 9, 15, 23, Y, Z - Please create a new function for finding X, Y, Z value
+<br/>- Please use “Place search|Place API(by Google)” for finding all restaurants in Bangsue
+area and show result by JSON
+<br/>- Please create one small project for Line messaging API(Up to you), contain a flow
+diagram, your code, and database.</h5>
+
+<br/><h3>Frontend</h3>
+    <h5>
+  -  Create a new router called “SCG”
+<br/>- Please use Bootstrap V4 for CSS
+<br/>- Please create Top bar, body, and footer for every page components
+<br/>- Please create a new page for showing results from your API(1.3)
+<br/>- One simple static page for your CV</h5>
+
+    </div>
+  );
+}
+
+export default Assignment;
