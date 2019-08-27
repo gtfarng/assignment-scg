b724931474107b403a5a9567b212abf8e88cb33c
gtfarng <5735512002@psu.ac.th>
2019-08-27 14:51:03 +0700

    update

--- frontend/src/Components/Assignment.js
+++ frontend/src/Components/Assignment.js
@@ -19,7 +19,7 @@ diagram, your code, and database.</h5>
     <h5>
     - Create a new router called “<Link to="/scg">SCG</Link>”
 <br/>- X, 5, 9, 15, 23, Y, Z - Please create a new <Link to="/function">function</Link> for finding X, Y, Z value
-<br/>- Please use Bootstrap V4 for CSS
+<br/>- Please use <a href="https://getbootstrap.com/docs/4.3/getting-started/introduction/">Bootstrap</a> V4 for CSS
 <br/>- Please create Top bar, body, and footer for every page components
 <br/>- Please create a new page for showing results from your API(1.3)
 <br/>- One simple static page for your <Link to="/cv">CV</Link>
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import logo from '../logo.svg';
+import react from '../logo.svg';
 
 function Home() {
 
@@ -15,9 +15,12 @@ function Home() {
       <br/><h1>Welcome!!!</h1>
         <br/>
 
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={react} className="App-logo" width="40%" alt="react" />
 
-        <br/><h5>React.JS (FrontEnd)</h5>
+        <br/><a href="https://reactjs.org/docs/getting-started.html"><h5>React.JS (FrontEnd)</h5></a>
+
+        <img src="https://miro.medium.com/max/1260/1*pxfq-ikL8zPE3RyGB2xbng.png" width="40%"  alt="node" />
+        <br/><a href="https://nodejs.org/en/docs"><h5>Node.JS (BackEnd)</h5></a>
     </div>
   );
 }
